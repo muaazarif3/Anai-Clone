2583bbf13534317cffe75eb5d935d6c983e74c3a
Muaa Arif <muaazarif3@gmail.com>
2024-06-25 09:49:54 -0700

    Hello world

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Anai-Clone/',  // Replace {repository-name} with your repository name
   plugins: [react()],
-  base: '/{Anai-Clone}/',  // Replace {repository-name} with your repository name
 });
